67e2710f33223098fff636da1441b0d500fbe370
Jack700103 <wqzl0852@leeds.ac.uk>
2026-05-02 07:32:02 +0000

    data

--- task2.py
+++ task2.py
@@ -1,14 +1,14 @@
 def main():
     try:
-        monthly_savings = input("Enter the amount you want to save each month: ")
+        monthly_savings = input()
 
         monthly_savings = int(monthly_savings)
 
         annual_savings = monthly_savings * 12
-        total_amount = annual_savings * 1.008  # 0.8% interest means multiply by 1.008
+        total_amount = annual_savings * 1.008  # 0.8% interest
 
-        print(f"You will save {annual_savings} per year.")
-        print(f"You will have {total_amount:.2f} in total after 1 year including interest.")
+        print(f"{annual_savings}")
+        print(f"£{total_amount:.2f}")
         
     except ValueError:
         print("Invalid amount")
