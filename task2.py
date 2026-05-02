e74b6963882699e67543cd6dd8506d8c3c161a5d
Jack700103 <wqzl0852@leeds.ac.uk>
2026-05-02 07:42:53 +0000

    data

--- task2.py
+++ task2.py
@@ -7,16 +7,19 @@ def main():
         monthly_input = line.strip()
 
         if not monthly_input:
-            raise ValueError("Empty input")
+            raise ValueError
 
         monthly_savings = int(monthly_input)
 
         annual_savings = monthly_savings * 12
-        total_amount = annual_savings * 1.008  # 0.8% interest
+        
+        total_amount = annual_savings * 1.008
 
         print(annual_savings)
+
         print(f"£{total_amount:.2f}")
         
     except ValueError:
         print("Invalid amount")
+
 main()
